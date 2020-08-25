c1129428ea2bbf5a1b5e2d24746dc799e1cee1be
MURARI <murarikumar42@gmail.com>
2020-08-25 08:06:24 +0530

    final proj

---
+++ shopping-cart/src/com/murari/dao/OrderDao.java
@@ -0,0 +1,19 @@
+package com.murari.dao;
+
+import java.util.List;
+
+import com.murari.beans.OrderBean;
+import com.murari.beans.TransactionBean;
+
+public interface OrderDao {
+	
+	public String paymentSuccess(String userName,double paidAmount);
+
+	public boolean addOrder(OrderBean order);
+	
+	public boolean addTransaction(TransactionBean transaction);
+	
+	public int countSoldItem(String prodId);
+	
+	public List<OrderBean> getAllOrders();
+}
---
+++ shopping-cart/src/com/murari/dao/TransDao.java
@@ -0,0 +1,6 @@
+package com.murari.dao;
+
+public interface TransDao {
+
+	public String getUserId(String transId);
+}
